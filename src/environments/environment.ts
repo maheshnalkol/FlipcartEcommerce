eb523732a95abe69c881be80361ef391fcfae729
maheshnalkol <maheshnalkol500@gmail.com>
2025-04-25 22:09:40 +0530

    2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseurl: 'https://ecom-backend-beta.vercel.app/',
+  baseurl: 'https://ecom-backend-8pvf1atxs-maheshnalkols-projects.vercel.app/',
 };
 
 /*
